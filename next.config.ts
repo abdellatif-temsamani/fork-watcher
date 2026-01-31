931bc7532e27da6ae6ea6fae2c0c45c3f865622e
abdellatif Temsamani <contact@abdellatif.dev>
2026-02-01 00:53:30 +0100

    feat: ppr

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        pathname: '/**',
-      },
-    ],
-  },
+    reactCompiler: true,
+    cacheComponents: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
